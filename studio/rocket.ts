3a8234684e27d6f550ceb503fff02bcdc5bf5d10
Jake Conwell <jakelconwell@gmail.com>
2023-03-02 10:20:23 -0600

    works as intended

---
+++ studio/rocket.ts
@@ -0,0 +1,52 @@
+import { Payload } from './Payload';
+import { Astronaut } from './astronaut';
+import { Cargo } from './cargo';
+
+export class Rocket {
+    name: string; totalCapacityKg: number; cargoItems: Cargo[] = []; astronauts: Astronaut[] = []; massKg: number;
+    constructor(name: string, totalCapacityKg: number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    };
+
+    sumMass( items: Payload[]): number {
+        let sum: number = 0;
+        
+        for(let i = 0; i < items.length; i++) {
+            sum += items[i].massKg
+        };
+        return sum;
+    };
+
+    currentMassKg() : number {
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+        
+    };
+
+    canAdd( item: Payload) : boolean {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    addCargo(cargo: Cargo): boolean {
+        if (this.canAdd(cargo)){
+            this.cargoItems.push(cargo);
+            return true;
+        } else {
+            return false;
+        };
+    };
+
+    addAstronaut(astronaut: Astronaut): boolean {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        } else {
+            return false;
+        };
+    };
+    
+}
